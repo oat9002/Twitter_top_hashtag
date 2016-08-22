c5e6150d9fc8346004eb0456c63845b0c8e79275
oato.o <oat9002@gmail.com>
2016-08-22 18:37:10 +0700

    Implemented count algorithm

--- controller/TwitterController.js
+++ controller/TwitterController.js
@@ -8,4 +8,16 @@ twitterRouter.route('/').get((req, res) => {
   res.send('<h1>Hello Twitter top hashtag service</h1>')
 })
 
+twitterRouter.route('/getHashtag').get((req, res) => {
+  TwitterService.getHashtag().then(hashtag => {
+    res.send(hashtag)
+  })
+})
+
+twitterRouter.route('/getRankHashtag').get((req, res) => {
+  TwitterService.getRankHashtag().then(rankHashtag => {
+    res.send(rankHashtag)
+  })
+})
+
 export default twitterRouter
--- service/TwitterService.js
+++ service/TwitterService.js
@@ -1,22 +1,64 @@
 import Twit from 'twit'
 import { db } from '../db'
 import cron from 'cron'
+import twitText from 'twitter-text'
 
 let cronJob = cron.CronJob
 
 export function getHashtag() {
   return new Promise((resolve, reject) => {
-    db.twitter.findOne({ tweetID: 767400495174398000 }, (err, document) => {
+    db.twitter.find((err, document) => {
       if(!err) {
-        
+        let hashtags = new Array(document.length)
+        document.forEach((item, index)=> {
+          let hashtag = twitText.extractHashtags(item.text)
+          hashtags[index] = hashtag
+        })
+        resolve(hashtags)
       }
       else {
         reject(err)
       }
     })
-   }
+  })
  }
 
+export function getRankHashtag() {
+  return new Promise((resolve, reject) => {
+    let topHashtags = []
+    getHashtag().then(hashtags => {
+      hashtags.forEach((arrHashtag, index) => {
+        if(index == 0) {
+          arrHashtag.forEach((hashtag) => {
+            let temp = {}
+            temp.text = hashtag
+            temp.count = 1
+            topHashtags.push(temp)
+          })
+        }
+        else {
+          arrHashtag.forEach(hashtag => {
+            topHashtags.forEach(topHashtag => {
+              if(hashtag === topHashtag.text) {
+                topHashtag.count = topHashtag.count + 1
+              }
+              else {
+                let temp = {}
+                temp.text = hashtag
+                temp.count = 1
+                topHashtags.push(temp)
+              }
+            })
+          })
+        }
+      })
+      resolve(topHashtags)
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
 //save tweets every 30 minutes
 // let saveTweetJob = new cronJob('* */30 * * * *', () => {
 //   getAllQuery().then(docs => {
